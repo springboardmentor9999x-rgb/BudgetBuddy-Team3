e72fcd843fc51c78984e858e80d74fd9c7aac983
Sanjuushree <sanjurajendran2007@gmail.com>
2026-09-08 17:54:25 +0530

    Empty main branch

--- backend/test_db.py
+++
@@ -1,8 +0,0 @@
-from app.database import engine
-
-try:
-    with engine.connect() as connection:
-        print("✅ PostgreSQL Database Connected Successfully!")
-except Exception as e:
-    print("❌ Database Connection Failed!")
-    print(e)
